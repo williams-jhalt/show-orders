39a4322c3a81d44c5e7f44e361b3c318020933d5
John Halterman <johnh@williams-trading.com>
2019-04-25 12:25:45 -0400

    added best seller import

--- src/AppBundle/Entity/Vendor.php
+++ src/AppBundle/Entity/Vendor.php
@@ -81,7 +81,7 @@ class Vendor {
      *
      * @var string
      * 
-     * @ORM\Column(name="email", type="string", length=255)
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
      */
     private $email;
 
